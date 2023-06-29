a1e55e43f4ef7dbd0e04680ec0ddced0bb115d36
StephenCodesStuff <122505441+StephenCodesStuff@users.noreply.github.com>
2023-06-29 14:22:35 -0500

    login stuff

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,7 +12,9 @@ router.get('/', async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/homepage');
+    res.redirect('/homepage', { 
+      logged_in: req.session.logged_in 
+    });
     return;
   }
 
