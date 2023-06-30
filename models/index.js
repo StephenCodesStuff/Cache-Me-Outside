e4a9072886362313917d42cb0372097fa88924e5
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-06-30 15:07:43 -0500

    typo corrected

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./users');
 const Caches = require('./caches');
-const FoundCaches = require('./foundCaches');
+const FoundCaches = require('./foundcaches');
 
 //User can create/hide many caches
 User.hasMany(Caches, {
