a5134bb86409a9faed643a48e5a86512a47e7eda
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-06-30 12:08:46 -0500

    caches displaying from db to hbs

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,25 +2,49 @@ const router = require('express').Router();
 const { User, Caches } = require('../models');
 const withAuth = require('../utils/auth');
 
-//GET homepage 
 router.get('/', async (req, res) => {
-  res.render('homepage', {logged_in: req.session.logged_in});
+  try {
+      const cacheData = await Caches.findAll(
+          {include: {model: User}}
+      );
+
+      const caches = cacheData.map((caches) =>
+      caches.get({ plain: true })
+      );
+
+      res.render('homepage', {
+          caches,
+          logged_in: req.session.logged_in
+      });
+
+  } catch (err) {
+    console.log(err);
+      res.status(500).json(err);
+  }
 });
 
-// router.get('/login', (req, res) => {
-//   // If the user is already logged in, redirect the request to another route
-//   if (req.session.logged_in) {
-//     res.redirect('/homepage', { 
-//       logged_in: req.session.logged_in 
-//     });
-//     return;
-//   }
-
-//   res.render('login');
+// //GET homepage 
+// router.get('/', async (req, res) => {
+//   res.render('homepage', {logged_in: req.session.logged_in});
 // });
+
+// // router.get('/login', (req, res) => {
+// //   // If the user is already logged in, redirect the request to another route
+// //   if (req.session.logged_in) {
+// //     res.redirect('/homepage', { 
+// //       logged_in: req.session.logged_in 
+// //     });
+// //     return;
+// //   }
+
+// //   res.render('login');
+// // });
 
 //GET profile page
 router.get('/profile', withAuth, async (req, res) => {
   res.render('profile', {logged_in: req.session.logged_in})
 });
+
+//GET all caches for testing purposes
+
 module.exports = router;
