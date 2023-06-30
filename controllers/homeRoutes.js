2a21aa4c30b8227c1f7f7b8f7401d9db77089a3e
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-06-30 10:28:30 -0500

    merge

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,11 +4,7 @@ const withAuth = require('../utils/auth');
 
 
 router.get('/', async (req, res) => {
-  res.render('homepage', { logged_in: req.session.logged_in});
-});
-
-router.get('/', async (req, res) => {
-  res.render('homepage');
+  res.render('homepage', {logged_in: req.session.logged_in});
 });
 
 router.get('/login', (req, res) => {
