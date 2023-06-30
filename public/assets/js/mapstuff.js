f255de6f4a985e75b240b0e3b33be35180107178
StephenCodesStuff <122505441+StephenCodesStuff@users.noreply.github.com>
2023-06-30 14:29:22 -0500

    map made

--- public/assets/js/mapstuff.js
+++ public/assets/js/mapstuff.js
@@ -1,19 +1,30 @@
-const displayMaps= async () => {
+const displayMaps= () => {
 
-const cacheData = await fetch(`/`, {
-    method: 'GET',
-    body: JSON.stringify({id, lat, lon}),
-    header: {
-        'Content-Type': 'application/json'
-    },
-})
-
-for (let index = 0; index < array.length; index++) {
-    let map = L.map(`map-id`).setView([41.994, -87.6602], 13);
+fetch(`/api/cache`).then(function (response) {
+    return response.json();
+}).then(function (data) {
+    for(let i = 0; i< data.length; i++) {
+        console.log(data[i]);
+        let map = L.map(`map-${data[i].id}`).setView([data[i].lat, data[i].lon], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 10,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    var marker = L.marker([41.994, -87.6602]).addTo(map);
+    var marker = L.marker([data[i].lat, data[i].lon]).addTo(map);
+    }
+})
+
+
+
+    
+
 }
-}
+
+displayMaps()
+
+// let map = L.map(`map-1`).setView([50, 50], 13);
+//     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//         maxZoom: 10,
+//         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+//     }).addTo(map);
+//     var marker = L.marker([50, 50]).addTo(map);
