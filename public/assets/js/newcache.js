d64e8ab1a8c43a81cae6bfb040515cef00a10b62
StephenCodesStuff <122505441+StephenCodesStuff@users.noreply.github.com>
2023-07-03 13:07:37 -0500

    getting data

--- public/assets/js/newcache.js
+++ public/assets/js/newcache.js
@@ -1,34 +1,43 @@
+const getCurrentLocation = () => {
+  return new Promise((resolve, reject) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const currentLocation = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          };
+          resolve(currentLocation);
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    } else {
+      reject(new Error("Geolocation is not supported by this browser."));
+    }
+  });
+};
+
 const cacheFormHandler = async (event) => {
   event.preventDefault();
 
-  // let currentLocation;
+ 
 
-  // function getCurrentLocation() {
-  //   if (navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition(function(position) {
-  //       currentLocation = {
-  //         latitude: position.coords.latitude,
-  //         longitude: position.coords.longitude
-  //       };
-  //       console.log("Current location:", currentLocation);
-  //     }, function(error) {
-  //       console.log("Error getting location:", error);
-  //     });
-  //   } else {
-  //     console.log("Geolocation is not supported by this browser.");
-  //   }
-  // }
+  let currentLocation;
 
-  // // Call the getCurrentLocation function to get the user's location
-  // getCurrentLocation();
+  try {
+    currentLocation = await getCurrentLocation();
+    console.log("Current location:", currentLocation);
+  } catch (error) {
+    console.log("Error getting location:", error);
+  }
 
-  // Collect values from the form
   const name = document.querySelector('#cache-name').value.trim();
   const hints = document.querySelector('#cache-hints').value.trim();
   const description = document.querySelector('#cache-description').value.trim();
   const difficulty = document.querySelector('#cache-difficulty').value;
-  console.log(name, hints, description, difficulty);
+  console.log(name, hints, description, difficulty, currentLocation);
 };
 
-document.querySelector('.cache-form').addEventListener('submit', cacheFormHandler);
-  
+document.querySelector('.new-cache-form').addEventListener('submit', cacheFormHandler);
