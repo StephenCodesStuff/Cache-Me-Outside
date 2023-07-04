23707a3702353aed370de30eb91a184c4a2dd905
KT <132851569+kumih0@users.noreply.github.com>
2023-07-04 06:35:28 -0500

    saving get all and get one cache in cache routes

--- controllers/api/cacheRoutes.js
+++ controllers/api/cacheRoutes.js
@@ -2,6 +2,38 @@ const router = require('express').Router();
 const { Caches, User } = require('../../models');
 const withAuth = require('../../utils/auth')
 
+//GET all caches for testing purposes
+router.get('/', async (req, res) => {
+  try {
+      const cacheData = await Caches.findAll(
+          {include: {model: User}}
+      );
+
+      const caches = cacheData.map((caches) =>
+      caches.get({ plain: true })
+      );
+      res.status(200).json(caches);
+  } catch (err) {
+    console.log(err);
+      res.status(500).json(err);
+  }
+});
+
+//GET one cache by id
+router.get('/:id', async (req, res) => {
+  try {
+      const cacheData = await Caches.findByPk(req.params.id, {
+          include: { model: User },
+      });
+    
+      const cache = cacheData.get({ plain: true });
+
+      res.status(200).json(cache);
+  } catch (err) {
+      res.status(500).json(err);
+  }
+});
+
 
 //CREATE new cache
 router.post('/', withAuth, async (req, res) => {
