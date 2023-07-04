c5d264f9b4b587d73094e916e571e3177b0c72cb
KT <132851569+kumih0@users.noreply.github.com>
2023-07-04 07:39:01 -0500

    get all found caches by user id

--- controllers/api/foundCacheRoutes.js
+++ controllers/api/foundCacheRoutes.js
@@ -20,3 +20,20 @@ router.get('/', async (req, res) => {
     }
 });
 
+//GET all found caches for a specific user
+router.get('/user/:id', async (req, res) => {
+    try {
+        const foundCacheData = await FoundCaches.findAll({
+            where: {finder_id: req.params.id},
+            include: {model: User, Caches}});
+    
+        const foundCaches = foundCacheData.map((foundCaches) =>
+        foundCaches.get({ plain: true })
+        );
+        console.log(`u got all found caches for user #${req.params.id}`);
+        res.status(200).json(foundCaches);
+    } catch (err) {
+        console.log("u broke it u moron", err);
+        res.status(500).json(err);
+    }
+});
