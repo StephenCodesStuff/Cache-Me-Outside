dc079664772f64664a63745a1aacbc040ad5d1f5
KT <132851569+kumih0@users.noreply.github.com>
2023-07-04 06:43:28 -0500

    get all found caches route for insomnia test

--- controllers/api/foundCacheRoutes.js
+++ controllers/api/foundCacheRoutes.js
@@ -1,1 +1,22 @@
 const router = require('express').Router();
+const { FoundCaches, User, Caches } = require('../../models');
+const withAuth = require('../../utils/auth')
+
+//GET all found caches for test in insomnia
+router.get('/', async (req, res) => {
+    try {
+        const foundCacheData = await FoundCaches.findAll(
+            {include: {model: User, Caches}}
+        );
+    
+        const foundCaches = foundCacheData.map((foundCaches) =>
+        foundCaches.get({ plain: true })
+        );
+        console.log("u done it stupid");
+        res.status(200).json(foundCaches);
+    } catch (err) {
+        console.log("u idiot u broke it", err);
+        res.status(500).json(err);
+    }
+});
+
