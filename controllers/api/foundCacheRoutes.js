210322e482f4a602787343d7c0fda4646501abfb
KT <132851569+kumih0@users.noreply.github.com>
2023-07-05 08:33:31 -0500

    recognizing mistake, correcting table/keys

--- controllers/api/foundCacheRoutes.js
+++ controllers/api/foundCacheRoutes.js
@@ -2,61 +2,61 @@ const router = require('express').Router();
 const { FoundCaches, User, Caches, TimesFound } = require('../../models');
 const withAuth = require('../../utils/auth')
 
-//GET all found caches for test in insomnia
-router.get('/', async (req, res) => {
-    try {
-        const foundCacheData = await FoundCaches.findAll({
-            include: [
-                {
-                    model: Caches,
-                    attributes: ['id', 'name', 'description', 'lat', 'lon', 'hider_id'],
-                },
-                {
-                    model: TimesFound,
-                    attributes: ['id', 'num_times_found', 'finder_id', 'foundcache_id'],
-                }
-            ]
-        }
-        );
+// //GET all found caches for test in insomnia
+// router.get('/', async (req, res) => {
+//     try {
+//         const foundCacheData = await FoundCaches.findAll({
+//             include: [
+//                 {
+//                     model: Caches,
+//                     attributes: ['id', 'name', 'description', 'lat', 'lon', 'hider_id'],
+//                 },
+//                 {
+//                     model: TimesFound,
+//                     attributes: ['id', 'num_times_found', 'finder_id', 'foundcache_id'],
+//                 }
+//             ]
+//         }
+//         );
 
-        const foundCaches = foundCacheData.map((foundCaches) =>
-            foundCaches.get({ plain: true })
-        );
-        console.log("u done it stupid");
-        res.status(200).json(foundCaches);
-    } catch (err) {
-        console.log("u idiot u broke it", err);
-        res.status(500).json(err);
-    }
-});
+//         const foundCaches = foundCacheData.map((foundCaches) =>
+//             foundCaches.get({ plain: true })
+//         );
+//         console.log("u done it stupid");
+//         res.status(200).json(foundCaches);
+//     } catch (err) {
+//         console.log("u idiot u broke it", err);
+//         res.status(500).json(err);
+//     }
+// });
 
-//GET all found caches for a specific user
-router.get('/user/:id', async (req, res) => {
-    try {
-        const foundCacheData = await FoundCaches.findAll({
-            where: { finder_id: req.params.id },
-            include: [
-                { 
-                    model: User,
-                    through: TimesFound,
-                    as: 'finders',
-                },
-                {
-                    model: Caches,
-                    attributes: ['id', 'name', 'description', 'lat', 'lon', 'hider_id'],
-                }]
-        });
+// //GET all found caches for a specific user
+// router.get('/user/:id', async (req, res) => {
+//     try {
+//         const foundCacheData = await FoundCaches.findAll({
+//             where: { finder_id: req.params.id },
+//             include: [
+//                 { 
+//                     model: User,
+//                     through: TimesFound,
+//                     as: 'finders',
+//                 },
+//                 {
+//                     model: Caches,
+//                     attributes: ['id', 'name', 'description', 'lat', 'lon', 'hider_id'],
+//                 }]
+//         });
 
-        const foundCaches = foundCacheData.map((foundCaches) =>
-            foundCaches.get({ plain: true })
-        );
-        console.log(`u got all found caches for user #${req.params.id}`);
-        res.status(200).json(foundCaches);
-    } catch (err) {
-        console.log("u broke it u moron", err);
-        res.status(500).json(err);
-    }
-});
+//         const foundCaches = foundCacheData.map((foundCaches) =>
+//             foundCaches.get({ plain: true })
+//         );
+//         console.log(`u got all found caches for user #${req.params.id}`);
+//         res.status(200).json(foundCaches);
+//     } catch (err) {
+//         console.log("u broke it u moron", err);
+//         res.status(500).json(err);
+//     }
+// });
 
 
 module.exports = router;
