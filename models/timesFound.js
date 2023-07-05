1b7db8c9aaa56c9472447148b7560075e6e72cd6
KT <132851569+kumih0@users.noreply.github.com>
2023-07-04 19:55:11 -0500

    created data table for timesfound

--- models/timesFound.js
+++ models/timesFound.js
@@ -2,3 +2,48 @@
 //foreign keys for foundcaches and finders, work as through table for M:M relationship between users and foundcaches
 //model to keep all finders information for each foundcache, ex. foundcache_id===cache_id, but each "find event" has a different finder/user
 //in order to save all past finders of a given cache, each event must be stored as a 'time-found' while the actual NUMBER of times the cache has been found is stored in the value inside 'times-found' column
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class TimesFound extends Model { }
+
+TimesFound.init(
+    {
+        //just the id for the instance that a cache was found
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        //actual number of times found
+        num_times_found: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        finder_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            },
+        },
+        foundcache_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'caches',
+                key: 'id'
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'timesfound'
+    },
+);
+
+module.exports = TimesFound;
