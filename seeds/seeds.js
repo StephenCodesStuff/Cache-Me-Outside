b5bdc6544723d0e12d8ab8994a1ac4353a7e6b3e
KT <132851569+kumih0@users.noreply.github.com>
2023-07-05 17:02:44 -0500

    holy shit it works, random gen timesfound seed!!

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -18,8 +18,26 @@ const seedDatabase = async () => {
     const caches = await Caches.bulkCreate(cacheData);
 
     //in order to have caches stored inside the found caches table, they must be found in (and the instance of "finding" recorded inside) the timesfound table
-
     
+    //declaring empty array for the all timesfound instance objects to be stored, essentially the .json file but dynamically created
+    const timesFound = [];
+    //creating a random number of timesfound instances
+    timesFound.length = Math.floor(Math.random() * 30 + 1);
+    console.log(timesFound.length);
+    //for loop within that array length to create seed data
+    for(let i = 0; i < timesFound.length; i++) {
+        timesFound[i] = await TimesFound.create({
+            id: i + 1,
+            //randomly assigning a user id to each instance
+            finder_id: users[Math.floor(Math.random() * users.length)].id,
+            //randomly assigning a cache id to each instance
+            cache_id: caches[Math.floor(Math.random() * caches.length)].id
+        });
+        console.log(timesFound[i], 'it worked idiot');
+    }
+    //mapping the timesfound array to extract data
+    const timesFoundSeed = timesFound.map((timefind) => timefind.get({ plain: true }));
+    console.log(timesFoundSeed);
 
 
 
