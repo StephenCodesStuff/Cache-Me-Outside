35252c1568dc29ace3e0983df10b3768d61ae283
KT <132851569+kumih0@users.noreply.github.com>
2023-07-05 17:04:16 -0500

    deleted foundcache timesfound json files & imports

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,11 +1,9 @@
 //import sequelize, models db tables
 const sequelize = require('../config/connection');
 const { User, Caches, FoundCaches, TimesFound } = require('../models');
-//import seed data for users, caches, found caches
+//import seed data for users, caches
 const userData = require('./userData.json');
 const cacheData = require('./cacheData.json');
-// const foundCacheData = require('./foundCacheData.json');
-// const timesFoundData = require('./timesFoundData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
