f8f077992452f5c7e4fbf7d4ff5ac374383f7316
KT <132851569+kumih0@users.noreply.github.com>
2023-07-04 20:15:39 -0500

    removing mistyped import

--- controllers/api/foundCacheRoutes.js
+++ controllers/api/foundCacheRoutes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const e = require('express');
 const { FoundCaches, User, Caches } = require('../../models');
 const withAuth = require('../../utils/auth')
 
