c3a5b76f890692e0e41e2cbd780b82b8e0ff6965
KT <132851569+kumih0@users.noreply.github.com>
2023-07-06 18:55:49 -0500

    forgot to load findcache script on expand hbs rip

--- public/assets/js/cache-forms/findCache.js
+++ public/assets/js/cache-forms/findCache.js
@@ -1,15 +1,20 @@
-const updateCache = async (cacheId, cache) => {
-    const response = await fetch(`/api/cache/${cacheId}`, {
-        method: 'PUT',
-        body: JSON.stringify(cache),
-        headers: { 'Content-Type': 'application/json' },
-    });
-    if (response.ok) {
-        alert('Cache found, updated successfully.');
-        document.location.replace('/profile');
-    } else {
-        alert(response.statusText);
+const updateCache = async () => {
+    if (event.target.matches('.found-btn')) {
+        const response = confirm('Did you really find the cache?');
+        cacheID = event.target.getAttribute('data-cache-id');
+        if (response) {
+            const response = await fetch(`api/cache/${cacheID}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+            });
+            if (response.ok) {
+                alert('Cache found, updated successfully.');
+                document.location.replace('/profile');
+            } else {
+                alert(response.statusText);
+            }
+        }
     }
-}
+};
 
-document.querySelector('.found-btn').addEventListener('click', updateCache);
+    document.querySelector('#found-btn').addEventListener('click', updateCache);
